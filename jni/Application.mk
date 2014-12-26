116ac22a9c7ba1e4e0dcb34fbe7595a37f6aa50c
luckyhacky <github@mail.my-cp.de>
2014-12-26 16:51:41 +0100

    fixed merge issue

--- jni/Application.mk
+++ jni/Application.mk
@@ -2,8 +2,4 @@ APP_MODULES := tincd
 APP_ABI := armeabi x86 mips
 #NDK_TOOLCHAIN_VERSION=4.8
 APP_PIE = true
-<<<<<<< HEAD
-APP_PLATFORM := android-10
-=======
 APP_PLATFORM := android-16
->>>>>>> bb562d8077d0dac746c16e3826a5cf88eee59c9f
